fb42c36d2b301fc060acf06716b1b4276da13e50
Jhunel B Penaflorida <jhunelpenaflorida@gmail.com>
2023-02-04 01:10:46 +0800

    Fix the zeroes.js

--- dist/zeroes.js
+++ dist/zeroes.js
@@ -1,24 +1,19 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const assert_1 = __importDefault(require("assert"));
 function endZeros(a) {
-    let stringNum = a.toString().split("");
+    let stringNum = a.toString().split("").reverse();
     let count = 0;
     for (let i = 0; i < stringNum.length; i++) {
         if (parseInt(stringNum[i]) === 0) {
             count++;
         }
+        else {
+            break;
+        }
     }
     return count;
 }
-console.log(endZeros(1));
+console.log(endZeros(100));
+console.log(endZeros(0));
 console.log(endZeros(1000));
-assert_1.default.strictEqual(endZeros(1), 0);
-assert_1.default.strictEqual(endZeros(0), 1);
-assert_1.default.strictEqual(endZeros(10), 1);
-assert_1.default.strictEqual(endZeros(100), 2);
-assert_1.default.strictEqual(endZeros(1000), 3);
+console.log(endZeros(1010));
 //# sourceMappingURL=zeroes.js.map
--- src/zeroes.ts
+++ src/zeroes.ts
@@ -1,23 +1,28 @@
-import assert from "assert";
+//import assert from "assert";
 
 function endZeros(a: number): number {
     // your code here
     
-        let stringNum = a.toString().split("");
+        let stringNum = a.toString().split("").reverse();
         let count = 0;
         for(let i = 0; i<stringNum.length;i++){
             if(parseInt(stringNum[i]) ===0){
                 count++;
             }
+            else{
+                break;
+            }
         }   
     return count;
 }
 
-console.log(endZeros(1))
+console.log(endZeros(100))
+console.log(endZeros(0))
 console.log(endZeros(1000))
+console.log(endZeros(1010))
 // These "asserts" are used for self-checking
-assert.strictEqual(endZeros(1), 0);
-assert.strictEqual(endZeros(0), 1);
-assert.strictEqual(endZeros(10), 1);
-assert.strictEqual(endZeros(100), 2);
-assert.strictEqual(endZeros(1000), 3);
+// assert.strictEqual(endZeros(1), 0);
+// assert.strictEqual(endZeros(0), 1);
+// assert.strictEqual(endZeros(10), 1);
+// assert.strictEqual(endZeros(100), 2);
+// assert.strictEqual(endZeros(1000), 3);
